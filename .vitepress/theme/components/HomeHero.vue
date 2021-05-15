d059bcc7c85f1df28d34d63010e9857e3d41930d
QC-L <github@liqichang.com>
2021-05-15 10:30:59 +0800

    docs(cn): index.md translation

--- .vitepress/theme/components/HomeHero.vue
+++ .vitepress/theme/components/HomeHero.vue
@@ -9,9 +9,7 @@
           <br />
         </p>
         <div class="description !-mt-4">
-          Presentation
-          <b>Sli</b>des for
-          <b>Dev</b>elopers
+          服务于<b>开发者们</b>的<b>演示文稿</b>
           <sup class="opacity-50">Beta</sup>
         </div>
 
@@ -31,7 +29,7 @@
 
         <div class="mt-5 flex">
           <div class="mx-auto">
-            <p>or try it now</p>
+            <p>或直接试用</p>
             <div class="language-bash mt-2">
               <pre><code><span class="opacity-50">$ </span><span class="token function">npm init</span> <span class="token text-[#408c9e] font-500">slidev</span></code></pre>
             </div>
