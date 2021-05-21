4f4e3b7a840eb618966094de4bc15cfb981b791a
QiChang Li <github@liqichang.com>
2021-05-21 19:06:36 +0800

    docs(cn): custom/config-vue translation (#47)

--- .vitepress/theme/components/Environment.vue
+++ .vitepress/theme/components/Environment.vue
@@ -7,16 +7,16 @@ defineProps<{ type: 'node' | 'client' }>()
 <template>
   <details class="px-4 py-3 mt-4 rounded bg-gray-400 bg-opacity-10">
     <summary class="outline-none">
-      Environment:
+      环境：
       <span class="capitalize" :class="type === 'node' ? 'text-orange-400' : 'text-green-400'">{{ type }}</span>
     </summary>
 
     <div class="mt-2 opacity-75">
       <span v-if="type === 'node'">
-        This setup function will only run on Node.js environment, you can have access to Node's API.
+        此配置仅在 Node.js 环境下运行。你可以访问 Node.js 的相关 API 了解更多。
       </span>
       <span v-else>
-        This setup function will only run on client side. Make sure the browser compacitblity when importing packages.
+        此配置仅在客户端环境下运行。在引入包时，请确保浏览器的兼容性。
       </span>
     </div>
   </details>
