f20f87e1ff27b59b2a9d4fddda02301dab200b96
Anthony Fu <anthonyfu117@hotmail.com>
2021-06-18 17:33:27 +0800

    Update config.js

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -213,7 +213,7 @@ module.exports = {
       indexName: 'slidev',
       searchParameters: {
         // for translations maintainers: change the filter to your locale code (subdomain name)
-        facetFilters: ['language:en']
+        facetFilters: ['language:cn']
       }
     },
 
