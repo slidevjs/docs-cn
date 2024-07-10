7a307e4f8dbe5c4672b456873b9509ff7a847567
_Kerman <kermanx@qq.com>
2024-07-10 23:17:28 +0800

    Discard changes to .vitepress/config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -96,6 +96,48 @@ const Addon: DefaultTheme.NavItemWithLink[] = [
   },
 ]
 
+const Translations = [
+  {
+    text: '简体中文',
+  },
+  {
+    text: 'English',
+    link: 'https://sli.dev{{pathname}}',
+  },
+  {
+    text: 'Français',
+    link: 'https://fr.sli.dev{{pathname}}',
+  },
+  {
+    text: 'Español',
+    link: 'https://es.sli.dev{{pathname}}',
+  },
+  {
+    text: 'Русский',
+    link: 'https://ru.sli.dev{{pathname}}',
+  },
+  {
+    text: 'Việt Nam',
+    link: 'https://vn.sli.dev{{pathname}}',
+  },
+  {
+    text: 'Deutsch',
+    link: 'https://de.sli.dev{{pathname}}',
+  },
+  {
+    text: 'Português (BR)',
+    link: 'https://br.sli.dev{{pathname}}',
+  },
+  {
+    text: 'Ελληνικά',
+    link: 'https://el.sli.dev{{pathname}}',
+  },
+  {
+    text: '日本語',
+    link: 'https://ja.sli.dev{{pathname}}',
+  },
+]
+
 const Customizations: (DefaultTheme.NavItemWithLink | DefaultTheme.NavItemChildren)[] = [
   {
     text: '自定义',
@@ -308,11 +350,11 @@ export default defineConfig({
 
   locales: {
     root: {
-      label: '简体中文',
+      label: 'English',
     },
     zh: {
-      label: 'English',
-      link: 'https://sli.dev/',
+      label: '简体中文',
+      link: 'https://cn.sli.dev/',
     },
     fr: {
       label: 'Français',
