d0426f1a09f096482ac9a519fda2f22c3d19bc52
_Kerman <kermanx@qq.com>
2024-07-22 12:33:51 +0800

    docs: translate `Environment` component

--- .vitepress/theme/components/Environment.vue
+++ .vitepress/theme/components/Environment.vue
@@ -5,19 +5,19 @@ defineProps<{ type: 'node' | 'client' | 'both' }>()
 <template>
   <details class="p4 mt-4 rounded-lg bg-gray-400 bg-opacity-10">
     <summary class="outline-none !m0 select-none">
-      Environment:
+      运行环境:
       <span class="capitalize font-bold" :class="type === 'node' ? 'text-orange-400' : 'text-green-400'">{{ type }}</span>
     </summary>
 
     <div class="pt2 opacity-75">
       <span v-if="type === 'both'">
-        This setup function will run on <b>both</b> Node.js and client side. Avoid using Node.js or DOM API to prevent runtime errors.
+        该文件中的代码将在 Node.js <b>和</b> 中浏览器 运行。请避免使用 Node.js 或 DOM API 以防止出现运行时错误。
       </span>
       <span v-else-if="type === 'node'">
-        This setup function will only run on Node.js environment, you can have access to Node's API.
+        该文件中的代码将仅在 Node.js 环境中运行，您可以访问 Node 的 API。
       </span>
       <span v-else-if="type === 'client'">
-        This setup function will only run on client side. Make sure the browser compatibility when importing packages.
+        该文件中的代码将仅在浏览器中运行。导入包时请确保浏览器兼容性。
       </span>
     </div>
   </details>
